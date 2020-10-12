ff6a2ab6aa329142afa1f1c42e57d5c7d081af4d
bogdan <bognan@mail.ru>
2020-10-12 22:49:49 +0300

    Доделал ДЗ 4, 5 и 6

--- HW/HW№4.py
+++ HW/HW№4.py
@@ -1,4 +1,8 @@
-my_list_1 = [2, 5, 8, 2, 12, 12, 4]
+my_list_1 = [1, 1, 1, 2, 2, 2, 2, 5, 8, 2, 12, 12, 4]
 my_list_2 = [2, 7, 12, 3]
 
-print(list(set(my_list_1) - set(my_list_2)))
+for number in my_list_1[:]:
+    if number in my_list_2:
+        my_list_1.remove(number)
+
+print(my_list_1)
--- HW/HW№6.py
+++ HW/HW№6.py
@@ -2,6 +2,8 @@ my_list_1 = [2, 2, 5, 12, 8, 2, 12]
 my_list_2 = []
 
 for number in my_list_1:
-    if number not in my_list_2:
+    if my_list_1.count(number) == 1:
         my_list_2.append(number)
+    else:
+        continue
 print(my_list_2)
