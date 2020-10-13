52d86e9f7aafef76dc18fbe6479824ba95b19629
bogdan <bognan@mail.ru>
2020-10-13 16:07:34 +0300

    Угадай число. Второе задание готово

--- guess the random namber/random number from user.py
+++ guess the random namber/random number from user.py
@@ -1,21 +1,22 @@
 import random
-
-number_comp = random.randint(1, 100)
+start_number = 1
+end_number = 100
+number_comp = random.randint(start_number, end_number)
 
 
 win = False
-num_list = []
 count = 0
 
 while not win:
     print(f'Вы загадали число: {number_comp}?')
-    num_list.append(number_comp)
     answ_user = str(input())
     if answ_user == '=':
         win = True
-        print(f'Компьютер победил')
+        print(f'Компьютер угадал c {count} попытки')
     elif answ_user == '>':
-        number_comp = random.randint(num_list[count], 100)
+        start_number = number_comp + 1
+        number_comp = random.randint(start_number, end_number)
     elif answ_user == '<':
-        number_comp = random.randint(1, num_list[count])
+        end_number = number_comp - 1
+        number_comp = random.randint(start_number, end_number)
     count += 1
